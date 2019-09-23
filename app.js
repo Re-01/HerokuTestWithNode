cc830b19a9f88e1853e1d4a75429f18284101db5
Cunha <r_cunha@ats-hr420-dm11.fc.ca>
2019-09-23 19:34:23 -0400

    updating final files

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const port = 3000;
+const path = require('path');
+
+// heroku assigns a port when it deploys via the process (environment variables - coming
+// locally this will run @ port 3000; remotely it will run wherever heroku tells it to run)
+const port = process.env.PORT || 3000; // a double pipe means "or"
 
 const app = express();
 
+app.use(express.static('public'));
+
+
+// a foward slash is the home rout
 app.get('/', (req,res) => {
   console.log('at the home route');
-  res.send('hello world!');
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
 app.get('/contact', (req,res) => {
   console.log('at the contact route');
-  res.send('ont the contact page!');
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
 })
 
 app.get('/portfolio', (req,res) => {
   console.log('at the portfolio route');
-  res.send('ont the portfolio page!');
+  res.sendFile(path.join(__dirname + '/views/portfolio.html'));
 })
 
 app.listen(port, () => {
